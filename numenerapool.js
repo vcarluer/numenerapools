3ec978020365b60f5ca2c8ecba1930845efe819b
Vincent Carluer <vcarluer@gmail.com>
2016-03-30 08:51:10 +0000

    base features

--- numenerapool.js
+++ numenerapool.js
@@ -8,10 +8,56 @@ function ready(fn) {
 
 var numenerapool = {
     run: function() {
-        this.mainDiv = document.getElementById("numenerapool");
-        this.mainDiv.innerHTML = "Hello 9 worlds";
+        this.mainDiv = document.getElementById('numenerapool');
+        this.pools = {};
+        this.createPool('might', 'Puissance');
+        this.createPool('speed', 'Célérité');
+        this.createPool('intellect', 'Intellect');
+    },
+    
+    createPool: function(poolName, poolCaption) {
+        var pool = new statPool();
+        this.pools[poolName] = pool;
+        pool.create(this.mainDiv, poolName, poolCaption);
     }
 };
+
+function statPool() {
+    this.create = function(div, css, name) {
+        this.name = name;
+        this.poolDiv = document.createElement('div');
+        this.poolDiv.innerHTML = this.name;
+        this.poolDiv.className = css;
+        div.appendChild(this.poolDiv);
+        
+        this.stat = 10;
+        this.minusDiv = document.createElement('div');
+        this.minusDiv.innerHTML = '-';
+        this.statDiv = document.createElement('div');
+        this.setStat();
+        this.addDiv = document.createElement('div');
+        this.addDiv.innerHTML = "+";
+        
+        this.poolDiv.appendChild(this.minusDiv);
+        this.poolDiv.appendChild(this.statDiv);
+        this.poolDiv.appendChild(this.addDiv);
+        
+        var self = this;
+        this.minusDiv.onclick = function() {
+            self.stat--;
+            self.setStat();
+        };
+        
+        this.addDiv.onclick = function() {
+            self.stat++;
+            self.setStat();
+        };
+    };
+    
+    this.setStat = function() {
+        this.statDiv.innerHTML = this.stat.toString();
+    };
+}
 
 ready(function() {
     numenerapool.run();   
