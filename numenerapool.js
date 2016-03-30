dc7a8b8a4dc3dcabbb66fd214c90f290e22b87b2
Vincent Carluer <vcarluer@gmail.com>
2016-03-30 11:58:01 +0000

    translations

--- numenerapool.js
+++ numenerapool.js
@@ -6,19 +6,47 @@ function ready(fn) {
   }
 }
 
+var fr = {
+    'might': 'Puissance',
+    'speed': 'Célérité',
+    'intellect': "Intellect"
+}
+
+var uk = {
+    'might': 'Might',
+    'speed': 'Speed',
+    'intellect': "Intellect"
+}
+
 var numenerapool = {
     run: function() {
         this.mainDiv = document.getElementById('numenerapool');
         this.pools = {};
-        this.createPool('might', 'Puissance');
-        this.createPool('speed', 'Célérité');
-        this.createPool('intellect', 'Intellect');
+        var language = this.getLanguage();
+        this.createPool('might', language);
+        this.createPool('speed', language);
+        this.createPool('intellect', language);
     },
     
-    createPool: function(poolName, poolCaption) {
+    getLanguage: function() {
+        var lang = getParameterByName('lang');
+        
+        var frenchFlag = document.getElementById('frImg');
+        var englishFlag = document.getElementById('ukImg');
+        
+        if (!lang || lang !== 'fr') {
+            englishFlag.style.opacity = 0.8;
+            return uk;
+        } else {
+            frenchFlag.style.opacity = 0.8;
+            return fr;    
+        }
+    },
+    
+    createPool: function(poolName, language) {
         var pool = new statPool();
         this.pools[poolName] = pool;
-        pool.create(this.mainDiv, poolName, poolCaption);
+        pool.create(this.mainDiv, poolName, language[poolName]);
     }
 };
 
@@ -79,6 +107,17 @@ function statPool() {
         this.statDiv.innerHTML = this.stat.toString();
         localStorage.setItem(this.css, this.stat);
     };
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    url = url.toLowerCase(); // This is just to avoid case sensitiveness  
+    name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 ready(function() {
